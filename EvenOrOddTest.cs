e74f62a1c398e7c56aaa2d73f8c337dd493d30eb
Lucas <lucas.fransson98@gmail.com>
2020-12-08 13:55:58 +0100

    Initial commit

---
+++ EvenOrOddTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odd_or_Even
+{
+    [TestFixture]
+    public class OddOrEvenTest
+    {
+        [Test]
+        public void Test_IsEven()
+        {
+            int[] arr = new int[] { 1, 2, 3 };
+            int SumOfArray = arr.Sum();
+
+            if (SumOfArray % 2 == 0)
+            {
+                Assert.Pass("Even");
+            }
+            else
+            {
+                Assert.Fail("Sum was not even");
+            }
+        }
+
+        [Test]
+        public void Test_IsOdd()
+        {
+            int[] arr = new int[] { 5, 5, 1 };
+            int SumOfArray = arr.Sum();
+
+            if (SumOfArray % 2 != 0)
+            {
+                Assert.Pass("Odd");
+            }
+            else
+            {
+                Assert.Fail("Sum was not odd");
+            }
+
+        }
+    }
+}
